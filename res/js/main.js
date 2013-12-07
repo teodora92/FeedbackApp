6b97bac5ef4e352d2ab29fa6e2aacf94aefe37ee
Teodora <teodora.petrisor92@gmail.com>
2013-12-07 18:52:09 +0100

    slideWscroll2

--- res/js/main.js
+++ res/js/main.js
@@ -715,7 +715,7 @@ function bindEvents() {
 				var target = e.target;
 				while (target.nodeType != 1) target = target.parentNode;
 
-				if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA' $$ target.tagName != 'RANGE')
+				if (target.tagName != 'SELECT' && target.tagName != 'INPUT' && target.tagName != 'TEXTAREA')
 					e.preventDefault();
 			}
 		});
